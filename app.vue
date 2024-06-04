4a833316654a8b5ab96404e778314cc9d3119ea9
David <david@rlabs.org>
2024-06-04 23:51:10 +0200

    add: header base

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <Header />
   </div>
 </template>
---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="lg:max-w-[1440px] bg-black">
+    <div class="lg:py-32 lg:px-20">
+      <div class="grid grid-cols-5">
+        <div class="col-span-3">
+          <h1 class="text-white text-8xl font-bold">
+            Welcome to our<br />
+            corner of the sea
+          </h1>
+          <p class="text-white mt-5 text-2xl">
+            Here at MW ENERGY SOLUTIONS, we're dedicated to keeping the heart of
+            your vessel beating strong. 
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
