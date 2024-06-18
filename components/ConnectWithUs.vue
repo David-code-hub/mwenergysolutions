6df76956e31968b34f8b946bbb202dda52ff7c86
David <david@rlabs.org>
2024-06-18 20:19:57 +0200

    add: make responsive

--- components/ConnectWithUs.vue
+++ components/ConnectWithUs.vue
@@ -1,17 +1,22 @@
 <template>
-  <div
-    class="xl:max-w-[1440px] w-full xl:w-[1440px] relative isolate bg-[#0E46A3]"
-  >
-    <div class="lg:py-32 lg:px-32 text-center">
+  <div class="xl:max-w-[1440px] w-full xl:w-[1440px] relative isolate bg-white">
+    <div class="lg:p-32 py-10 px-5 text-center z-10 relative">
       <h1
-        class="text-white leading-tight text-7xl font-bold uppercase switzer-font header-text"
+        class="text-black leading-tight lg:text-7xl text-3xl font-bold uppercase switzer-font header-text"
       >
         Ready to get onboard?
       </h1>
-      <p class="text-white mt-3 text-xl font-normal">
+      <p class="text-base-grey mt-3 lg:text-xl text-sm font-normal mb-12">
         Join Us for Smooth Sailing and Exceptional Service
       </p>
+      <a
+        class="text-md lg:py-4 lg:px-6 py-2 px-3 text-center bg-[#0E46A3] text-white rounded-full border-white border-solid border-2 font-normal"
+        >Explore More</a
+      >
     </div>
+    <!-- <div
+      class="bg-[url('@/assets/images/mission.avif')] bg-black/60 bg-blend-multiply bg-cover bg-fixed h-full w-full absolute z-2 top-0 bg-top fixed"
+    ></div> -->
   </div>
 </template>
 
--- components/Header.vue
+++ components/Header.vue
@@ -3,35 +3,39 @@
     class="xl:max-w-[1440px] w-full xl:w-[1440px] relative isolate overflow-hidden bg-blue-circular-gradient"
   >
     <Navigation />
-    <div class="lg:py-60 lg:px-32">
+    <div class="lg:py-60 lg:px-32 py-10 px-5">
       <div class="grid grid-cols-5 text-center">
         <div class="col-span-5 z-10 relative text-center">
           <h1
-            class="text-white leading-tight text-8xl font-bold uppercase switzer-font header-text"
+            class="text-white leading-tight lg:text-8xl text-5xl lg:font-bold font-semibold uppercase switzer-font header-text"
           >
-            Welcome to our<br />
+            Welcome to our<br class="lg:block hidden" />
             corner of the
             <span class="text-outline-medium outlined-text">sea.</span>
           </h1>
           <h1
-            class="text-white leading-tight text-8xl font-bold uppercase switzer-font absolute top-0 w-full"
+            class="text-white leading-tight lg:text-8xl text-5xl lg:font-bold font-semibold uppercase switzer-font absolute top-0 w-full"
           >
-            Welcome to our<br />
+            Welcome to our<br class="lg:block hidden" />
             corner of the
             <span class="text-outline-medium wavey-text">sea.</span>
           </h1>
           <!--sub text-->
-          <p class="text-white mt-5 text-xl font-normal">
-            Here at MW ENERGY SOLUTIONS, we're dedicated to keeping<br />
+          <p class="text-white mt-5 lg:text-xl text-sm font-normal">
+            Here at MW ENERGY SOLUTIONS, we're dedicated to keeping<br
+              class="lg:block hidden"
+            />
             the heart of your vessel beating strong. 
           </p>
-          <div class="grid grid-cols-2 gap-5 w-fit my-10 mx-auto">
+          <div
+            class="grid lg:grid-cols-2 lg:w-fit w-full gap-5 w-fit lg:my-10 mb-20 mt-10 mx-auto"
+          >
             <a
-              class="text-lg py-3 px-5 text-center bg-white rounded-full border-white border-solid border-2 font-normal"
+              class="text-lg lg:py-3 lg:px-5 py-2 px-3 text-center bg-white rounded-full border-white border-solid border-2 font-normal"
               >Explore More</a
             >
             <a
-              class="text-lg text-center py-3 px-5 bg-transparent rounded-full border-white border-solid border-2 text-white font-normal"
+              class="text-lg text-center lg:py-3 lg:px-5 py-2 px-3 bg-transparent rounded-full border-white border-solid border-2 text-white font-normal"
               >Get Quote</a
             >
           </div>
@@ -44,10 +48,10 @@
     <!--bg-[#0E46A3]-->
 
     <div
-      class="backdrop-blur-md bg-white/5 border-solid border-y-2 border-white py-8 px-20 z-10 absolute bottom-[0em] w-[110vw] rotate-[0deg] overflow-hidden left-[-1em]"
+      class="backdrop-blur-md bg-white/5 border-solid border-y-2 border-white lg:py-8 py-4 px-20 z-10 absolute bottom-[0em] w-[110vw] rotate-[0deg] overflow-hidden left-[-1em]"
     >
       <p
-        class="uppercase text-white text-4xl flex font-bold switzer-font text-nowrap marquee"
+        class="uppercase text-white lg:text-4xl text-xl flex lg:font-bold font-medium switzer-font text-nowrap marquee"
       >
         Our Highly Skilled Engineers are readily available Worldwide • 24/7 •
         365 days
